33461e3ae7f4b5a048076ce65ddfa6d50b7fd7f6
Juan David <juandavidenriquez602@gmail.com>
2026-06-22 11:52:45 -0500

    corregi CORS

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ app = FastAPI(
 # En producción cambia allow_origins al dominio exacto de Vercel.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[cfg.frontend_url, "https://acv-rehab-proyecto.vercel.app/", "http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[cfg.frontend_url, "https://acv-rehab-proyecto.vercel.app", "http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
